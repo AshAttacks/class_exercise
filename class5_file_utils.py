743ec58d4db8181bd2d3d880cc4e3ff2bd552a3e
Richy <rxie0405@gmail.com>
2026-09-25 14:47:51 -0400

    Added class5_file_utils & class5_file_inspector

---
+++ class5_file_utils.py
@@ -0,0 +1,39 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def inspect_file(filepath_str):
+    """Return basic information about an existing file."""
+    # TODO 1: Create a Path object.
+    filepath = Path(filepath_str)
+    name = filepath.name
+    suffix = filepath.suffix
+    # TODO 2: If the path is not a file:
+    #         Log an ERROR message.
+    #         Raise FileNotFoundError (e.g. file not found).
+    if not filepath.exists():
+        logger.error(f"File not found: {filepath_str}")
+        raise FileNotFoundError()
+    # TODO 3: Return a dictionary containing:
+    #         name and extension.
+    dict = {'name' : name, 'extension' : suffix}
+    return dict
+
+
+def inspect_extension(file_info):
+    """Confirm that the file uses a supported text extension."""
+    supported_extension = ".txt"
+
+    # TODO 4: If file_info["extension"] does not equal
+    #         supported_extension:
+    #         Log an ERROR message (e.g. unsupported format).
+    #         Raise ValueError.
+
+    if not file_info['extension'] == supported_extension:
+        logger.error(f'File has an unsupported extension: {file_info['extension']}')
+        raise ValueError()
+
+    # TODO 5: Return file_info.
+    return file_info
